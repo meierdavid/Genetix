0b8baad8f9bbc4ef796f51ad22ee801600b1293a
meierdavid <davidmeier@hotmail.fr>
2018-03-15 10:32:00 +0100

    Supr basic project ( WTF JOSS)

--- views/sequences/interSeq.php
+++ views/sequences/interSeq.php
@@ -20,7 +20,7 @@ use yii\widgets\ActiveForm;
         <form method="post" action="#" onsubmit="return false;">
             <label for="sequence">Enter a sequence</label>&nbsp;:&nbsp;<input type="text" class="post" style="width: 320px" id="sequence" name="sequence" onkeyup="chargeAjax('#resultat', 'interSeq.php?sequence='+encodeURI($('#sequence').val()));" />
             
-             <input type="submit" name="submit" class="btn btn-success" value="Interpréter" onclick="chargeAjax('#resultat', 'interSeq.php?sequence='+$('#sequence').val());" class='mainoption'/>
+             <input type="submit" name="submit" class="btn btn-success" value="Interpret" onclick="chargeAjax('#resultat', 'interSeq.php?sequence='+$('#sequence').val());" class='mainoption'/>
             
         </form>
     </div> 
